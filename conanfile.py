250b96e82abadfde0e3421b0ebb24840df0c4130
Thabo Krick <thabo.krick@btc-es.de>
2023-12-18 22:35:37 +0100

    updated conan recipe, fixed model settings

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,3 @@
-from os.path import join
-
 from conan import ConanFile
 from conan.tools.files import copy
 
@@ -10,8 +8,8 @@ class MyPackage(ConanFile):
     license = "MIT"
     url = "https://github.com/thabok/deps-shared-mod-a"
     description = "Conan package for Shared Module A"
-    exports_sources = "shared_module_a.slx"  # This line includes your file in the package
+    exports_sources = "*.slx"
     no_copy_source = False
 
     def package(self):
-        copy(self, "*.slx", self.source_folder, self.package_folder)
+        copy(self, "*.slx", self.source_folder, self.package_folder, keep_path=False)
